e8300fd174373587584ea00f16de4bedd5c32137
todina-r <ratsinjomananatolotra@gmail.com>
2026-04-23 07:42:07 +0300

    ex0

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,36 @@
+from typing import IO
+from sys import argv
+
+
+def try_get_file(file_name: str) -> IO[str] | None:
+    file: IO[str] | None = None
+    try:
+        file = open(file_name)
+    except Exception as error:
+        print(f"Error opening file '{file_name}':", error)
+    finally:
+        return file
+
+
+def print_file_content(file: IO[str]) -> None:
+    content: str = file.read()
+    print("---\n")
+    print(content)
+    print("---")
+
+
+def recover(file_name: str):
+    file: IO[str] | None = try_get_file(file_name)
+    if file:
+        print(f"Accessing file '{file_name}'")
+        print_file_content(file)
+        file.close()
+        print(f"File '{file_name}' closed.")
+
+
+if __name__ == "__main__":
+    print("=== Cyber Archives Recovery ===")
+    if len(argv) == 2:
+        recover(argv[1])
+    else:
+        print("Usage: ft_ancient_text.py <file>")
