2cc1e17692bcb5871f565da90633af3657703af4
todina-r <ratsinjomananatolotra@gmail.com>
2026-07-14 20:44:23 +0300

    output format improv

--- ex0/ft_ancient_text.py
+++ ex0/ft_ancient_text.py
@@ -6,31 +6,41 @@ def try_get_file(file_name: str) -> IO[str] | None:
     file: IO[str] | None = None
     try:
         file = open(file_name)
+    except FileNotFoundError:
+        print(f"[Error] - file not found '{file_name}'")
+    except PermissionError:
+        print(f"[Error] - unreadable file '{file_name}'")
+    except IsADirectoryError:
+        print(f"[Error] - is a directory '{file_name}'")
     except Exception as error:
-        print(f"Error opening file '{file_name}':", error)
-    finally:
-        return file
+        print(f"[Error] - failed to open file '{file_name}':", error)
+    return file
 
 
 def print_file_content(file: IO[str]) -> None:
     content: str = file.read()
-    print("---\n")
+    print("=== BEGIN")
     print(content)
-    print("---")
+    print("=== END")
 
 
 def recover(file_name: str) -> None:
     file: IO[str] | None = try_get_file(file_name)
     if file:
-        print(f"Accessing file '{file_name}'")
+        print(f"[LOG] - Accessing file '{file_name}'\n")
         print_file_content(file)
+        print(f"\n[Log] - closing file '{file_name}'...")
         file.close()
-        print(f"File '{file_name}' closed.")
+        print(f"[LOG] - File '{file_name}' closed.")
 
 
-if __name__ == "__main__":
-    print("=== Cyber Archives Recovery ===")
+def main() -> None:
+    print("=== Cyber Archives Recovery ===\n")
     if len(argv) == 2:
         recover(argv[1])
     else:
-        print("Usage: ft_ancient_text.py <file>")
+        print("Usage: python ft_ancient_text.py <file>")
+
+
+if __name__ == "__main__":
+    main()
