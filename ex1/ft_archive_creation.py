bf2cf92f733294386a961d1893073306a9306ff0
todina-r <ratsinjomananatolotra@gmail.com>
2026-07-15 08:28:12 +0300

    decrease the number of time a file is opened

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -1,8 +1,6 @@
 from typing import IO
 from sys import argv
 
-# TODO: used function verification
-
 
 def try_get_file(file_name: str, mode: str = "r") -> IO[str] | None:
     file: IO[str] | None = None
@@ -19,64 +17,58 @@ def try_get_file(file_name: str, mode: str = "r") -> IO[str] | None:
     return file
 
 
-def print_file_content(file: IO[str], suffix: str = "") -> None:
+def print_content(content: str, suffix: str = "") -> str:
+    new_content: str = content
+    if suffix != "":
+        new_content = content.replace("\n", suffix + "\n")
     print("=== BEGIN")
-    for line in file:
-        print(line[:-1] + suffix)
+    print(new_content)
     print("=== END")
+    return new_content
 
 
-def copy_file(src_name: str, dst_name: str) -> None:
-    new_content: str = ""
-    dst: IO[str] | None = None
-    src: IO[str] | None = try_get_file(src_name)
-    if src:
-        for line in src:
-            new_content += line[:-1] + "#" + "\n"
-        src.close()
-        dst = try_get_file(dst_name, "w")
-        if dst:
-            dst.write(new_content)
-            print(f"[LOG] - Data saved in file '{dst_name}'")
-            print(f"\n[LOG] - Closing file '{dst_name}'")
-            dst.close()
-            print(f"[LOG] - File '{dst_name}' closed")
-
-
-def recovery(file_name: str) -> None:
+def recovery(file_name: str) -> str | None:
     file: IO[str] | None = try_get_file(file_name)
+    content: str | None = None
     if file:
         print(f"[LOG] - Reading file '{file_name}'")
-        print_file_content(file)
+        content = print_content(file.read())
         print(f"\n[LOG] - Closing file '{file_name}'")
         file.close()
         print(f"[LOG] - File '{file_name}' closed")
+    return content
 
 
-def preserv(file_name: str) -> None:
-    file: IO[str] | None = try_get_file(file_name)
+def preserv(content: str) -> None:
     new_file_name: str = ""
-    if file:
-        print("\n[LOG] - Transforming data")
-        print_file_content(file, "#")
-        print("\nEnter new file name (or empty): ", end="")
-        try:
-            new_file_name = input()
-        except BaseException:
-            pass
-        if new_file_name != "":
-            print(f"\n[LOG] - Saving data to '{new_file_name}'")
-            copy_file(file_name, new_file_name)
-        else:
-            print("\n[LOG] - Not saving data")
-        file.close()
+    new_file: IO[str] | None
+    print("\n[LOG] - Transforming data")
+    new_content: str = print_content(content, "#")
+    print("\nEnter new file name (or empty): ", end="")
+    try:
+        new_file_name = input()
+    except BaseException:
+        pass
+    if new_file_name != "":
+        print(f"\n[LOG] - Saving data to '{new_file_name}'")
+        new_file = try_get_file(new_file_name, "w")
+        if new_file:
+            new_file.write(new_content)
+            print(f"[LOG] - Data saved in file '{new_file_name}'")
+            print(f"\n[LOG] - Closing file '{new_file_name}'")
+            new_file.close()
+            print(f"[LOG] - File '{new_file_name}' closed")
+    else:
+        print("\n[LOG] - Not saving data")
 
 
 if __name__ == "__main__":
+    content: str | None
     print("=== Cyber Archives Recovery & Preservation ===\n")
     if len(argv) == 2:
         file_name: str = argv[1]
-        recovery(file_name)
-        preserv(file_name)
+        content = recovery(file_name)
+        if content:
+            preserv(content)
     else:
         print("Usage: python ft_archive_creation.py <file>")
