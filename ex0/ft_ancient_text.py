69b915a1e8532021418f2fbf8cb0008a5fd22d09
todina-r <ratsinjomananatolotra@gmail.com>
2026-07-15 16:13:13 +0300

    structure and readability improvement

--- ex0/ft_ancient_text.py
+++ ex0/ft_ancient_text.py
@@ -5,7 +5,7 @@ from sys import argv
 def try_get_file(file_name: str) -> IO[str] | None:
     file: IO[str] | None = None
     try:
-        file = open(file_name)
+        file = open(file_name, encoding="utf-8")
     except FileNotFoundError:
         print(f"[Error] - file not found '{file_name}'")
     except PermissionError:
@@ -19,31 +19,37 @@ def try_get_file(file_name: str) -> IO[str] | None:
 
 def print_file_content(file: IO[str]) -> bool:
     content: str = ""
+    success: bool = False
     try:
         content = file.read()
         print("=== BEGIN ===")
         print(content)
         print("==== END ====")
-        return True
+        success = True
     except UnicodeDecodeError:
         print("[Error] - invalid file encoding")
-        return False
+    except Exception as error:
+        print("[Error] - failed to read file:", error)
+    return success
 
 
 def recover(file_name: str) -> None:
     file: IO[str] | None = try_get_file(file_name)
     if file:
-        print(f"[LOG] - Accessing file '{file_name}'\n")
-        print_file_content(file)
+        print(f"[Log] - Accessing file '{file_name}'\n")
+        if not print_file_content(file):
+            print(f"[Error] - failed to read file '{file_name}'")
         print(f"\n[Log] - closing file '{file_name}'...")
         file.close()
-        print(f"[LOG] - File '{file_name}' closed.")
+        print(f"[Log] - File '{file_name}' closed.")
 
 
 def main() -> None:
+    file_name: str = ""
     print("=== Cyber Archives Recovery ===\n")
     if len(argv) == 2:
-        recover(argv[1])
+        file_name = argv[1]
+        recover(file_name)
     else:
         print("Usage: python ft_ancient_text.py <file>")
 
