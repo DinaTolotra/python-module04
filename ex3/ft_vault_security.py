d8c2dcddabd5347a1847f508e671cbf8364c5f31
todina-r <ratsinjomananatolotra@gmail.com>
2026-07-16 10:52:04 +0300

    add ex3 mode validation in secure_archive()

--- ex3/ft_vault_security.py
+++ ex3/ft_vault_security.py
@@ -5,11 +5,16 @@ def secure_archive(
     success: bool = True
     try:
         with open(file_name, mode) as file:
-            if content:
+            if content and mode == "r":
                 file.write(content)
                 content = "Content successfully written to file"
-            else:
+            elif mode == "w":
                 content = file.read()
+            else:
+                raise ValueError(
+                    f"Invalid mode '{mode}'.\n" +
+                    "Valid options are: 'r' or 'w'"
+                )
     except Exception as error:
         content = str(error)
         success = False
@@ -22,21 +27,25 @@ def main() -> None:
 
     print("=== Cyber Archives Security ===")
 
-    print("\nUsing 'secure_archive' to read from a nonexistent file:")
-    print(secure_archive("/not/existing/file"))
+    try:
+        print("\nUsing 'secure_archive' to read from a nonexistent file:")
+        print(secure_archive("/not/existing/file"))
 
-    print("\nUsing 'secure_archive' to read from an inaccessible file:")
-    print(secure_archive("/etc/shadow"))
+        print("\nUsing 'secure_archive' to read from an inaccessible file:")
+        print(secure_archive("/etc/shadow"))
 
-    print("\nUsing 'secure_archive' to read from a directory:")
-    print(secure_archive("/etc"))
+        print("\nUsing 'secure_archive' to read from a directory:")
+        print(secure_archive("/etc"))
 
-    print("\nUsing 'secure_archive' to read from a regular file:")
-    (success, content) = secure_archive("file")
-    print((success, content))
+        print("\nUsing 'secure_archive' to read from a regular file:")
+        (success, content) = secure_archive("file")
+        print((success, content))
 
-    print("\nUsing 'secure_archive' to write previous content to a new file:")
-    print(secure_archive("new_file", "w", content))
+        print("\nUsing 'secure_archive'",
+              "to write previous content to a new file:")
+        print(secure_archive("new_file", "w", content))
+    except ValueError as e:
+        print("[Error] -", e)
 
 
 if __name__ == "__main__":
